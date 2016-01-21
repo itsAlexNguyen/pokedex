4d5f0a685aef3ce14cd348b99f70abde0df6f448
Alex Nguyen <alexnguyen@alexs-macbook-pro.local>
2016-01-21 17:02:34 -0500

    Initial Commit

---
+++ pokedex-by-Alex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-Alex
+//
+//  Created by Alex Nguyen on 2016-01-21.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
