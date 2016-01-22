f6256d5eca029c50427715e46f04b1c7f1b54b97
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-01-22 12:44:58 -0500

    Added details view UI and started adding more properties to Pokemon Object

--- PokemonDetailVC.swift
+++ PokemonDetailVC.swift
@@ -6,8 +6,29 @@
 //  Copyright © 2016 Alex Nguyen. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
 class PokemonDetailVC: UIViewController {
-
+    var pokemon: Pokemon!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UIView!
+    @IBOutlet weak var baseAtkLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    @IBOutlet weak var descLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nameLabel.text = pokemon.name.capitalizedString;
+    }
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 }
