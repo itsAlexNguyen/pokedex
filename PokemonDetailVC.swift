d4630e51f094bd2d1dca717991bc439656efaa51
Alex Nguyen <alexnguyen@Alexs-MacBook-Pro.local>
2016-01-22 11:38:10 -0500

    Added Pokemon details segue

---
+++ PokemonDetailVC.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex-by-Alex
+//
+//  Created by Alex Nguyen on 2016-01-22.
+//  Copyright © 2016 Alex Nguyen. All rights reserved.
+//
+
+import Cocoa
+
+class PokemonDetailVC: UIViewController {
+
+}
